1493808c2dafaf452e0b1c62fe57a202ece6a62e
Barbara Masinde <barbzlornah@gmail.com>
2020-08-18 17:49:30 +0300

    Add project files.

---
+++ CRUDoperation/Controllers/CRUDController.cs
@@ -0,0 +1,120 @@
+﻿using CRUDoperation;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUDoperations.Controllers
+{
+    public class CRUDController : Controller
+    {
+        private TestDBEntities db = new TestDBEntities();
+
+        //  
+        // GET: /CRUD/ 
+        public ActionResult Index()
+        {
+            return View(db.Employees.ToList());
+        }
+        
+
+        public ActionResult Details(string id = null)
+        {
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        //  
+        // GET: /CRUD/Create  
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //  
+        // POST: /CRUD/Create  
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(employee);
+        }
+
+        //  
+        // GET: /CRUD/Edit/5  
+
+        public ActionResult Edit(string id = null)
+        {
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        //  
+        // POST: /CRUD/Edit/5  
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        //  
+        // GET: /CRUD/Delete/5  
+
+        public ActionResult Delete(string id = null)
+        {
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        //  
+        // POST: /CRUD/Delete/5  
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Employee employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
